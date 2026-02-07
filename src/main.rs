d7ea8b27eb40efc0c7dd1a90226cdbe20eebdd45
Giorgi Grigalashvili <71205861+BUGO07@users.noreply.github.com>
2026-02-07 23:03:01 +0400

    add deps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use bevy::prelude::*;
+use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins((
+            DefaultPlugins,
+            EguiPlugin::default(),
+            WorldInspectorPlugin::default(),
+        ))
+        .add_systems(Startup, setup)
+        .add_systems(Update, update)
+        .run();
 }
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera3d::default());
+}
+fn update() {}
